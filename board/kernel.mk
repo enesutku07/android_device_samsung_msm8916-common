40f9e8c5b5f348041b353735f047f707e101e23e
enesutku07 <afyonluenes@hotmail.com>
2018-11-11 02:20:45 +0300

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,11 +1,13 @@
 # Kernel
-BOARD_KERNEL_CMDLINE += \
-	console=null \
-	androidboot.hardware=qcom \
-	user_debug=23 \
-	msm_rtb.filter=0x3F \
-	ehci-hcd.park=3 \
-	androidboot.bootdevice=7824900.sdhci
+#BOARD_KERNEL_CMDLINE += \
+#	console=null \
+#	androidboot.hardware=qcom \
+#	user_debug=23 \
+#	msm_rtb.filter=0x3F \
+#	ehci-hcd.park=3 \
+#	androidboot.bootdevice=7824900.sdhci
+
+BOARD_KERNEL_CMDLINE := selinux=0
 
 BOARD_CUSTOM_BOOTIMG_MK := $(PLATFORM_PATH)/mkbootimg.mk
 BOARD_DTBTOOL_ARGS := -2
@@ -15,6 +17,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
+#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+#TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
